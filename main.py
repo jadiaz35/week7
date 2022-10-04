caa41c306f8d601a45dd4d9865f9ac12ed866e1b
Juan Diaz <113930348+jadiaz35@users.noreply.github.com>
2022-10-04 17:33:59 +0000

    loop

--- main.py
+++ main.py
@@ -7,11 +7,29 @@
 # Random in python
 # List comprehension
 #importing modules
+#modularized your code
 #work on creating and understanding functions
 
 from decision_making import decisions
 from comparison_operators import comparison
+from loops import loop
 # from dictionaries_loops import dictionaries_loops
+#from enumerators import enumerator
+# from list_comprehension import list_comprehensions
+# from logical_operators import logical_operator
+# from min_max import minmax
+# from random import randoms
+# from ranges import range
+# from while_loops import whileloop 
 
 decisions()
 comparison()
+loop()
+
+#enumerator()
+#list_comprehensions()
+# logical_operator()
+# minmax()
+# randoms()
+# range()
+# whileloop()
